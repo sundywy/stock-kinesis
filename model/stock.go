87780445b632db64fd93a6252f18a713c4cc8c8a
Sundy W Yaputra <sundy.yap@gmail.com>
2018-05-22 01:00:29 +0800

    Initial commit

---
+++ model/stock.go
@@ -0,0 +1,66 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type TradeType int
+
+const (
+	Buy TradeType = iota + 1
+	Sell
+)
+
+func (t TradeType) String() (s string) {
+	switch t {
+	case Buy:
+		s = "Buy"
+	case Sell:
+		s = "Sell"
+	}
+	return
+}
+
+type StockTrade struct {
+	TickerSymbol string    `json:"ticker_symbol"`
+	TradeType    TradeType `json:"trade_type"`
+	Price        float64   `json:"price"`
+	Quantity     int       `json:"quantity"`
+	ID           int       `json:"id"`
+}
+
+func New(tickerSymbol string, tradeType TradeType, price float64, quantity, id int) *StockTrade {
+	return &StockTrade{
+		TickerSymbol: tickerSymbol,
+		TradeType:    tradeType,
+		Price:        price,
+		Quantity:     quantity,
+		ID:           id,
+	}
+}
+
+func NewFromBytes(data []byte) *StockTrade {
+	var s StockTrade
+
+	if err := json.Unmarshal(data, &s); err != nil {
+		return nil
+	}
+	return &s
+}
+
+func (s *StockTrade) String() string {
+
+	return fmt.Sprintf(`ID: %d, %s %d shares of %s as of %.02f`,
+		s.ID, s.TradeType.String(), s.Quantity, s.TickerSymbol, s.Price)
+}
+
+func (s *StockTrade) AsBytes() []byte {
+
+	bytes, err := json.Marshal(s)
+	if err != nil {
+		return []byte{}
+	}
+
+	return bytes
+}
---
+++ writer/generator.go
@@ -0,0 +1,90 @@
+package writer
+
+import (
+	"math"
+	"math/rand"
+
+	"github.com/sundywy/stock-kinesis/model"
+)
+
+type stockPrice struct {
+	TickerSymbol string
+	Price        float64
+}
+
+type StockTradeGenerator struct {
+	stocks []*stockPrice
+}
+
+func New() *StockTradeGenerator {
+	data := map[string]float64{
+		"AAPL":  119.72,
+		"XOM":   91.56,
+		"GOOG":  527.83,
+		"BRK.A": 223999.88,
+		"MSFT":  42.36,
+		"WFC":   54.21,
+		"JNJ":   99.78,
+		"WMT":   85.91,
+		"CHL":   66.96,
+		"GE":    24.64,
+		"NVS":   102.46,
+		"PG":    85.05,
+		"JPM":   57.82,
+		"RDS.A": 66.72,
+		"CVX":   110.43,
+		"PFE":   33.07,
+		"FB":    74.44,
+		"VZ":    49.09,
+		"PTR":   111.08,
+		"BUD":   120.39,
+		"ORCL":  43.40,
+		"KO":    41.23,
+		"T":     34.64,
+		"DIS":   101.73,
+		"AMZN":  370.56,
+	}
+
+	var g StockTradeGenerator
+	for k, v := range data {
+		g.stocks = append(g.stocks, &stockPrice{TickerSymbol: k, Price: v})
+	}
+
+	return &g
+}
+
+func (g *StockTradeGenerator) Get(id int) *stockPrice {
+	return g.stocks[id]
+}
+
+func (g *StockTradeGenerator) Len() int {
+	return len(g.stocks)
+}
+
+const (
+	maxDeviation    = 0.2
+	maxQuantity     = 10000
+	probabilitySell = 0.4
+)
+
+func (g *StockTradeGenerator) GetRandomTrade() *model.StockTrade {
+
+	id := rand.Intn(g.Len())
+	stockPrice := g.Get(id)
+
+	deviation := (rand.Float64() - 0.5) * 2.0 * maxDeviation
+	price := stockPrice.Price * (1 + deviation)
+	price = math.Round(price*100.0) / 100.0
+
+	var tradeType model.TradeType
+
+	if rand.Float64() < probabilitySell {
+		tradeType = model.Sell
+	} else {
+		tradeType = model.Buy
+	}
+
+	quantity := rand.Intn(maxQuantity) + 1
+
+	return model.New(stockPrice.TickerSymbol, tradeType, price, quantity, id)
+}
